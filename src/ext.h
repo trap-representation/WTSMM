e6505ceea5a8d452c75fbc9c5a461ed4b80a4b64
Somdipto Chakraborty <108862434+trap-representation@users.noreply.github.com>
2025-05-06 14:40:45 +0530

    Updated things (read README)

--- src/ext.h
+++ src/ext.h
@@ -1,6 +1,14 @@
 #ifndef EXT_H
 #define EXT_H
 
+enum process {
+  SKIP_NONE,
+  SKIP_DEFAULT,
+  SKIP_CREATESCRIPTS,
+  SKIP_EXTSTART
+};
+
+#define init_f(statepath, restorepath, changepath) __declspec(dllexport) enum process init(char *statepath, char *restorepath, char *changepath)
 #define start_f(statepath, restorepath, changepath) __declspec(dllexport) unsigned int start(char *statepath, char *restorepath, char *changepath)
 
 #endif
